a8f44a1ed54f7723890059620e894ab3424940e2
ggitHub-312 <74096103+ggitHub-312@users.noreply.github.com>
2021-01-31 10:59:18 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -9,7 +9,7 @@ class Paper{
         this.x=x;
         this.y=y;
         this.r = r;
-        this.image = loadImage("sprites/paper.png")
+        this.image = loadImage("paper.png")
         this.body = Bodies.circle(this.x,this.y,this.r/2,options)
         World.add(world,this.body);
     }
